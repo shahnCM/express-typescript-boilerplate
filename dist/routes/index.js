8ac747a31afcea67094ac8b6ca194f1bd307593b
Mahir-BS23-21J7 <mahir.shahrier@brainstation-23.com>
2021-11-22 19:27:04 +0600

    fix: ignoreing dist folder for remote origin

--- dist/routes/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.webRoutes = exports.apiRoutes = void 0;
-var api_1 = require("./api");
-Object.defineProperty(exports, "apiRoutes", { enumerable: true, get: function () { return api_1.routes; } });
-var web_1 = require("./web");
-Object.defineProperty(exports, "webRoutes", { enumerable: true, get: function () { return web_1.routes; } });
-//# sourceMappingURL=index.js.map
